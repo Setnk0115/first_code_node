d181832b7d34d38959034bd6975027268355e0d7
Sebastian Ramirez Castrillon <sebas.ramirez.0115@gmail.com>
2025-10-17 06:58:16 -0500

    conatdor de multiplos del 3

--- main.js
+++ main.js
@@ -2,8 +2,9 @@ console.log("Hola mundo");
 
 //crear un programa que identifique cuantos multiplos de 3 ahi en un rango del 0 al 100 y que imprima
 
-for (let i = 1; i <= 100; i++) {
+for (let i = 0; i <= 100; i++) {
     if (i % 3 == 0) {
+        console.log(i)
     }
-    console.log(i)
+    
 }
