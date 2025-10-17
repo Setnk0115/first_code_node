2fd896023276a4fa16e39186362bd0b92127795f
Sebastian Ramirez Castrillon <sebas.ramirez.0115@gmail.com>
2025-10-17 06:55:18 -0500

    mi papá no de llama edgar

--- main.js
+++ main.js
@@ -2,10 +2,8 @@ console.log("Hola mundo");
 
 //crear un programa que identifique cuantos multiplos de 3 ahi en un rango del 0 al 100 y que imprima
 
-for (let i = 1; i < 100; i++) {
-    let M = 0;
-    if (i % 3 === 0) {
-        M++;
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0) {
     }
+    console.log(i)
 }
-console.log(`${M}`)
